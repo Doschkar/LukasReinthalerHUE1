448ed7f8542ba5ca29751d19103004e476b9b588
Doschkar <reinthalerl190255@sus.htl-grieskirchen.at>
2022-09-25 10:55:22 +0200

    Everything finished

--- LukasReinthalerHUE1Projekt/src/net/htlgkr/pos3/lukasReinthalerHUE1Projekt/EratosthenesPrimeSieve.java
+++ LukasReinthalerHUE1Projekt/src/net/htlgkr/pos3/lukasReinthalerHUE1Projekt/EratosthenesPrimeSieve.java
@@ -2,32 +2,29 @@ package net.htlgkr.pos3.lukasReinthalerHUE1Projekt;
 
 public class EratosthenesPrimeSieve implements PrimeSieve{
 
-    void sieveOfEratosthenes(int n)
-    {
-        boolean prime[] = new boolean[n + 1];
-        for (int i = 0; i <= n; i++)
-            prime[i] = true;
-
-        for (int p = 2; p * p <= n; p++) {
-            if (prime[p] == true) {
-                for (int i = p * p; i <= n; i += p)
-                    prime[i] = false;
-            }
-        }
-
-        for (int i = 2; i <= n; i++) {
-            if (prime[i] == true)
-                System.out.print(i + " ");
-        }
-    }
+    public String primes = "";
 
     @Override
     public boolean isPrime(int p) {
-        return false;
+        if (p < 2) {
+            return false;
+        }
+        if(p == 2){
+            primes = primes + p + ", ";
+            return true;
+        }
+        for (long i = 2; i * i <= p; i++) {
+            if (p % i == 0) {
+                return false;
+            }
+        }
+        primes = primes + p +", ";
+        return true;
     }
 
     @Override
     public void printPrimes() {
-
+        System.out.println("Die Primzahlen lauten:");
+        System.out.println(primes);
     }
 }
--- LukasReinthalerHUE1Projekt/src/net/htlgkr/pos3/lukasReinthalerHUE1Projekt/Main.java
+++ LukasReinthalerHUE1Projekt/src/net/htlgkr/pos3/lukasReinthalerHUE1Projekt/Main.java
@@ -6,12 +6,13 @@ public class Main {
 
     public static void main(String args[])
     {
+        EratosthenesPrimeSieve e = new EratosthenesPrimeSieve();
         Scanner s = new Scanner(System.in);
         System.out.println("Bis wohin sollen die Primzahlen berechnet werden?");
         int n = s.nextInt();
-        System.out.print("Following are the prime numbers ");
-        System.out.println("smaller than or equal to " + n);
-        EratosthenesPrimeSieve e = new EratosthenesPrimeSieve();
-        e.sieveOfEratosthenes(n);
+        for(int i = 1; i <= n; i++){
+            e.isPrime(i);
+        }
+        e.printPrimes();
     }
 }
